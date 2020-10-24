57c9ce0c896cb884e8431b9bb10120397708cb27
Diqin Yang <ydq9743@gmail.com>
2020-10-24 11:44:30 +0200

    test_RemoveBetsInCard_Works TDD2

--- AssignmentG3/src/test/java/CashierTest.java
+++ AssignmentG3/src/test/java/CashierTest.java
@@ -1,8 +1,21 @@
+import casino.cashier.Cashier;
+import casino.cashier.GamblerCard;
+import gamblingauthoritiy.BetLoggingAuthority;
+import gamblingauthoritiy.IBetLoggingAuthority;
 import org.junit.Test;
 
+import static org.mockito.Mockito.mock;
+
 public class CashierTest {
+    IBetLoggingAuthority loggingAuthority= new BetLoggingAuthority();
+    Cashier cashier = new Cashier(loggingAuthority);
+
     @Test
     public void test_RemoveBetsInCard_Works(){
-        throw new RuntimeException();
+        //Arrange
+        GamblerCard gamblerCard = mock(GamblerCard.class);
+
+        cashier.returnGamblerCard(gamblerCard);
+        //throw new RuntimeException();
     }
 }
