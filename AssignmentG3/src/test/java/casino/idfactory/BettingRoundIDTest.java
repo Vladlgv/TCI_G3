a16bd3d881a1db1bd1b4fc7f7e3bed151bbefc85
Vlad <gv.luca@gmail.com>
2020-10-25 12:17:33 +0100

    added more tests for IDs

--- AssignmentG3/src/test/java/casino/idfactory/BettingRoundIDTest.java
+++ AssignmentG3/src/test/java/casino/idfactory/BettingRoundIDTest.java
@@ -8,18 +8,50 @@ import java.util.UUID;
 import static org.junit.Assert.*;
 
 public class BettingRoundIDTest {
+    GeneralID bettingRoundID;
+
     ///
     //Test to see if betting Round ID is properly generated
     ///
     @Test
     public void test_BettingRoundID_GeneratesNumber() {
         //arrange
-        GeneralID bettingRoundID;
         //act
         bettingRoundID = (BettingRoundID) IDFactory.generateID("BettingRoundID");
         //assert
         Assertions.assertThat(bettingRoundID.getUniqueID()).isInstanceOf(UUID.class);
         Assertions.assertThat(bettingRoundID.getTimeStamp()).isNotNull();
         Assertions.assertThat(bettingRoundID).isNotNull();
+    }
+
+    ///
+    //Test to see if betting round  ID is properly generated
+    ///
+    @Test
+    public void test_CreateGamingMachineID_GamingMachineIDIsCreatedWithout_IDFactory() {
+        //arrange
+        //act
+        bettingRoundID = new BettingRoundID();
+        //assert
+        Assertions.assertThat(bettingRoundID.getUniqueID()).isInstanceOf(UUID.class);
+        Assertions.assertThat(bettingRoundID.getTimeStamp()).isNotNull();
+        Assertions.assertThat(bettingRoundID).isNotNull();
+    }
+
+    ///
+    //Test to see if betting round ID is properly generated and has different UUID
+    ///
+    @Test
+    public void test_CreateGamingMachineID_GamingMachineHasDifferentUUID() {
+        //arrange
+        BettingRoundID b1 = new BettingRoundID();
+        BettingRoundID b2 = new BettingRoundID();
+        //act
+        bettingRoundID = new BettingRoundID();
+        //assert
+        Assertions.assertThat(bettingRoundID.getUniqueID()).isNotSameAs(b1.getUniqueID());
+        Assertions.assertThat(bettingRoundID.getUniqueID()).isNotSameAs(b2.getUniqueID());
+        Assertions.assertThat(b1.getUniqueID()).isNotSameAs(b2.getUniqueID());
+
     }
 }
--- AssignmentG3/src/test/java/casino/idfactory/GamingMachineIDTest.java
+++ AssignmentG3/src/test/java/casino/idfactory/GamingMachineIDTest.java
@@ -9,18 +9,50 @@ import java.util.UUID;
 import static org.junit.Assert.*;
 
 public class GamingMachineIDTest {
+    GeneralID gamingMacineID;
     ///
-    //Test to see if betting Round ID is properly generated
+    //Test to see if game machine ID is properly generated
     ///
     @Test
     public void test_CreateGamingMachineID_GamingMachineIDIsCreated() {
         //arrange
-        GeneralID gamingMacineID;
         //act
         gamingMacineID = (GamingMachineID) IDFactory.generateID("GamingMachineID");
         //assert
         Assertions.assertThat(gamingMacineID.getUniqueID()).isInstanceOf(UUID.class);
         Assertions.assertThat(gamingMacineID.getTimeStamp()).isNotNull();
         Assertions.assertThat(gamingMacineID).isNotNull();
+    }
+
+    ///
+    //Test to see if game machine ID is properly generated
+    ///
+    @Test
+    public void test_CreateGamingMachineID_GamingMachineIDIsCreatedWithout_IDFactory() {
+        //arrange
+        GeneralID gamingMacineID;
+        //act
+        gamingMacineID = new GamingMachineID();
+        //assert
+        Assertions.assertThat(gamingMacineID.getUniqueID()).isInstanceOf(UUID.class);
+        Assertions.assertThat(gamingMacineID.getTimeStamp()).isNotNull();
+        Assertions.assertThat(gamingMacineID).isNotNull();
+    }
+
+    ///
+    //Test to see if game machine ID is properly generated and has different UUID
+    ///
+    @Test
+    public void test_CreateGamingMachineID_GamingMachineHasDifferentUUID() {
+        //arrange
+        GamingMachineID g2 = new GamingMachineID();
+        GamingMachineID g3 = new GamingMachineID();
+        //act
+        gamingMacineID = new GamingMachineID();
+        //assert
+        Assertions.assertThat(gamingMacineID.getUniqueID()).isNotSameAs(g2.getUniqueID());
+        Assertions.assertThat(gamingMacineID.getUniqueID()).isNotSameAs(g3.getUniqueID());
+        Assertions.assertThat(g2.getUniqueID()).isNotSameAs(g3.getUniqueID());
+
     }
 }
